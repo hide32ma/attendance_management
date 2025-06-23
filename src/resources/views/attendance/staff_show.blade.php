bda2ff1189a662a80f53fb93156db93e528d5fa1
hide32 <hide32.1219@gmail.com>
2025-06-23 18:10:10 +0900

    勤怠詳細ページ 休憩データ+1個表示

--- src/resources/views/attendance/staff_show.blade.php
+++ src/resources/views/attendance/staff_show.blade.php
@@ -29,40 +29,45 @@
 
     <!-- 日付 -->
     <label>日付</label>
-    {{ $attendance->work_date ?? $workDate }}
+    {{ \Carbon\Carbon::parse($attendance->work_date ?? $workDate)->format('Y年n月j日') }}
+
 
 
     <!-- 出勤 -->
-    <label>出勤</label>
+    <label>出勤・退勤</label>
     <input type="datetime-local" name="clock_in"
         value="{{ optional($attendance)->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format('Y-m-d\TH:i') : '' }}">
 
     <span>〜</span>
 
     <!-- 退勤 -->
-    <label>退勤</label>
+    <!-- <label>退勤</label> -->
     <input type="datetime-local" name="clock_out"
         value="{{ optional($attendance)->clock_out ? \Carbon\Carbon::parse($attendance->clock_out)->format('Y-m-d\TH:i') : '' }}">
 
     <!-- 休憩開始 -->
-    <label>休憩</label>
-    <input type="datetime-local" name="breaks[0][start]"
-        value="{{ optional($attendance->breakTimes[0] ?? null)->break_start
-                    ? \Carbon\Carbon::parse($attendance->breakTimes[0]->break_start)->format('Y-m-d\TH:i') : '' }}">
+    @php
+    $breakTimes = $attendance->breakTimes ?? collect();
+    $count = $breakTimes->count();
+    @endphp
 
-    <span>〜</span>
+    @for ($i = 0; $i < $count + 1; $i++)
+        <label>
+        {{ $i === 0 ? '休憩' : '休憩' . ($i + 1) }}
+        </label>
+        <input type="datetime-local" name="breaks[{{ $i }}][start]"
+            value="{{ isset($breakTimes[$i]) ? \Carbon\Carbon::parse($breakTimes[$i]->break_start)->format('Y-m-d\TH:i') : '' }}">
+        <span>〜</span>
+        <input type="datetime-local" name="breaks[{{ $i }}][end]"
+            value="{{ isset($breakTimes[$i]) ? \Carbon\Carbon::parse($breakTimes[$i]->break_end)->format('Y-m-d\TH:i') : '' }}">
+    @endfor
 
-    <!-- 休憩終了 -->
-    <!-- <label>休憩</label> -->
-    <input type="datetime-local" name="breaks[0][end]"
-        value="{{ optional($attendance->breakTimes[0] ?? null)->break_end
-                    ? \Carbon\Carbon::parse($attendance->breakTimes[0]->break_end)->format('Y-m-d\TH:i') : '' }}">
 
-    <!-- 申請理由 -->
-    <label>申請理由 :</label>
-    <textarea name="reason"></textarea>
 
-    <button type="submit">修正</button>
+        <!-- 申請理由 -->
+        <label>申請理由 :</label>
+        <textarea name="reason"></textarea>
+        <button type="submit">修正</button>
 </form>
 
 
